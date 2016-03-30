9374dd1946b5a0624fbcc4135ad3557e35058ff8
Andrew Bauer <abau171@gmail.com>
2016-03-30 11:56:52 -0400

    added proof generation

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def state_str(state):
+	return " ".join(str(num) for num in state)
+
+class ProofEntry:
+	def __init__(self, operation, argument, calculation, result):
+		self.operation = operation
+		self.argument = argument
+		self.calculation = calculation
+		self.result = result
+
+proof = []
+
 left_cache = {}
 right_cache = {}
 
@@ -11,13 +23,30 @@ def L(state):
 
 def L_uncached(state):
 	if len(state) == 1:
-		return state[0]
+		result = state[0]
+		proof.append(ProofEntry("L", str(state[0]), None, result))
+		return result
 	elif len(state) == 2:
 		a, b = state[0], state[1]
-		return b if a < b else 0
+		if a < b:
+			result = b
+			proof.append(ProofEntry("L", state_str(state), None, result))
+			return result
+		else:
+			result = 0
+			proof.append(ProofEntry("L", state_str(state), None, result))
+			return result
 	else:
 		w, b = state[:-1], state[-1]
-		return L(w) - R(w) + b if b > R(w) else 0
+		l, r = L(w), R(w)
+		if b > r:
+			result = l - r + b
+			proof.append(ProofEntry("L", state_str(state), "{} - {} + {}".format(l, r, b), result))
+			return result
+		else:
+			result = 0
+			proof.append(ProofEntry("L", state_str(state), None, result))
+			return result
 
 def R(state):
 	if state in right_cache:
@@ -29,13 +58,30 @@ def R(state):
 
 def R_uncached(state):
 	if len(state) == 1:
-		return state[0]
+		result = state[0]
+		proof.append(ProofEntry("R", str(state[0]), None, result))
+		return result
 	elif len(state) == 2:
 		a, b = state[0], state[1]
-		return a if b < a else 0
+		if b < a:
+			result = a
+			proof.append(ProofEntry("R", state_str(state), None, result))
+			return result
+		else:
+			result = 0
+			proof.append(ProofEntry("R", state_str(state), None, result))
+			return result
 	else:
 		a, w = state[0], state[1:]
-		return R(w) - L(w) + a if a > L(w) else 0
+		l, r = L(w), R(w)
+		if a > l:
+			result = r - l + a
+			proof.append(ProofEntry("R", state_str(state), "{} - {} + {}".format(r, l, a), result))
+			return result
+		else:
+			result = 0
+			proof.append(ProofEntry("R", state_str(state), None, result))
+			return result
 
 def get_outcome_class(l, r):
 	if l == 0:
@@ -53,3 +99,11 @@ if __name__ == "__main__":
 	print("outcome class " + outcome_class)
 	print("L(w) = " + str(l))
 	print("R(w) = " + str(r))
+	show_proof = input("show proof (y/n)? ")
+	if show_proof == "y":
+		print("proof:")
+		for entry in proof:
+			if entry.calculation != None:
+				print("{}({}) = {} = {}".format(entry.operation, entry.argument, entry.calculation, entry.result))
+			else:
+				print("{}({}) = {}".format(entry.operation, entry.argument, entry.result))
